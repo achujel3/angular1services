8ea5e263ff2d35e93be0855f9bc1be5ac8e60002
achujelas123 <achujel3@gmail.com>
2017-05-08 18:36:38 +0300

    last commit

--- contact.controller.js
+++ contact.controller.js
@@ -3,10 +3,55 @@
     var app = angular.module("contactApp");
     app.controller("ContactCtrl", ContactCtrl);
 
-    function ContactCtrl(ContactDataSvc) {
-        this.contacts = ContactDataSvc.contacts;
+    function ContactCtrl(ContactDataSvc, $http) {
+        var self = this;
+        self.editMode = false;
+        ContactDataSvc.getContacts()
+            .then(function(data) {
+                self.contacts = data;
+            });
+
         this.selectContact = function(index) {
             this.selectedContact = this.contacts[index];
+            self.successMessage = undefined;
+            self.errorMessage = undefined;
+        }
+
+        this.toggleEditMode = function() {
+            this.editMode = !self.editMode;
+        }
+
+        this.saveUser = function() {
+            this.toggleEditMode();
+            var userData = this.selectedContact;
+
+            if (this.addMode) {
+                ContactDataSvc.createUser(userData)
+                    .then(function() {
+                            self.successMessage = "This USER has been updated!";
+                        },
+                        function() {
+                            self.errorMessage = "Error!";
+                        });
+                self.addMode = false;
+            } else {
+                ContactDataSvc.saveUser(userData)
+                    .then(function() {
+                            self.successMessage = "This USER has been updated!";
+                        },
+                        function() {
+                            self.errorMessage = "Error!";
+                        });
+            }
+
+        }
+
+        this.addContact = function() {
+            self.addMode = true;
+            this.selectedContact = {
+                "id": new Date().toTimeString()
+            }
+            this.editMode = true;
         }
     }
 })();
